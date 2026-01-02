1b1dff91dcc6ed5b04705e1a2a3014f7d835a59b
Sabyata Karki <sabyatakarki05@com>
2026-01-02 16:55:01 +0545

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mini_grocery/app/app.dart';
-void main(){
-  runApp(App());
-}
+import 'package:mini_grocery/core/services/hive/hive_service.dart';
+import 'package:mini_grocery/core/services/storage/user_session_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 4. Set System UI Styles
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+  // Initialize Hive Service
+  final hiveService = HiveService();
+  await hiveService.init();
+  // 5. Initialize SharedPreferences instance
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  runApp(
+    ProviderScope(
+      overrides: [
+        // Inject initialized SharedPreferences into the provider
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: 
+      const App(),
+    ),
+  );
+}
